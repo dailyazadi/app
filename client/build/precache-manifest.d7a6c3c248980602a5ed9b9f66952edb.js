70c3aa3a9875df09fab7c23e332fd92a4053eb63
Muatasim Qazi <muatasimqazi@gmail.com>
2019-08-31 19:44:25 -0700

    include build folder

---
+++ client/build/precache-manifest.d7a6c3c248980602a5ed9b9f66952edb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9f663168187481c7a959837093e001",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001c0dfead986e0e5163",
+    "url": "/static/css/main.281fba37.chunk.css"
+  },
+  {
+    "revision": "35893b568c40f8bae88d",
+    "url": "/static/js/2.a123aa84.chunk.js"
+  },
+  {
+    "revision": "001c0dfead986e0e5163",
+    "url": "/static/js/main.1aae1f4b.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  },
+  {
+    "revision": "4b37da11a19bd60a9432a7603aada419",
+    "url": "/static/media/jameel-noori-nastaleeq.4b37da11.ttf"
+  },
+  {
+    "revision": "b64196850a1b856fcdee02ab68add9c2",
+    "url": "/static/media/logo.b6419685.svg"
+  }
+]);
