17cd7563e24e5c3ca5ac52d3ad057c1e23bdf2c5
Muatasim Qazi <muatasimqazi@gmail.com>
2019-09-22 00:49:33 +0000

    deploy static site @ Sun Sep 22 00:49:33 UTC 2019

---
+++ precache-manifest.904cc4d8f95043279a9348bef555d64f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abca1ce7ae6c7b0b037dae705b2cb95d",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "05187f93ecf05dacb988",
+    "url": "/app/static/css/main.afe0893b.chunk.css"
+  },
+  {
+    "revision": "35893b568c40f8bae88d",
+    "url": "/app/static/js/2.a123aa84.chunk.js"
+  },
+  {
+    "revision": "05187f93ecf05dacb988",
+    "url": "/app/static/js/main.ead0f8ed.chunk.js"
+  },
+  {
+    "revision": "68bdcd177901dafd4706",
+    "url": "/app/static/js/runtime~main.2995df3e.js"
+  },
+  {
+    "revision": "4b37da11a19bd60a9432a7603aada419",
+    "url": "/app/static/media/jameel-noori-nastaleeq.4b37da11.ttf"
+  },
+  {
+    "revision": "b64196850a1b856fcdee02ab68add9c2",
+    "url": "/app/static/media/logo.b6419685.svg"
+  }
+]);
